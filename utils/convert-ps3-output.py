85282d9d72d40414cd53b0b44b5e28eb76ab05bc
xperia64 <xperiancedapps@gmail.com>
2021-12-22 16:02:27 -0500

    Update to python3 proper

--- utils/convert-ps3-output.py
+++ utils/convert-ps3-output.py
@@ -3,27 +3,27 @@ import sys
 
 def main():
     if len(sys.argv) <= 2:
-        print "This script generates the .expected file from your PS3's debug logs."
-        print ""
-        print "Usage:   convert-ps3-output.py <input> <output>"
-        print "Example: convert-ps3-output.py hello_world.log hello_world.expected"
+        print("This script generates the .expected file from your PS3's debug logs.")
+        print("")
+        print("Usage:   convert-ps3-output.py <input> <output>")
+        print("Example: convert-ps3-output.py hello_world.log hello_world.expected")
         return False
 
     #Parse and check arguments
     inputFile  = sys.argv[1]
     outputFile = sys.argv[2]
     if not os.path.isfile(inputFile):
-        print "[!] Input file does not exist"
+        print("[!] Input file does not exist")
         return False
 
     f = open(inputFile, 'rb')
     w = open(outputFile, 'wb')
 
     data = f.read()
-    data = data[data.find("/app_home/"):]
-    data = data[data.find("\x0D\x0A")+2:]
-    data = data[:data.rindex("END LOG")-12]
-    data = data.replace("\x0D\x0A", "\x0A")
+    data = data[data.find(b"/app_home/"):]
+    data = data[data.find(b"\x0D\x0A")+2:]
+    data = data[:data.rindex(b"END LOG")-12]
+    data = data.replace(b"\x0D\x0A", b"\x0A")
     
     w.write(data)
     w.close()
